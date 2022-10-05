d65854c3619e8c78a6d96686189dedeb36a2c015
Hongwei Chen <hong.chen@futurewei.com>
2022-10-04 22:37:32 -0700

    jest issue - opne hndler workaround

--- smart-caching/icn-protocol/service.test.js
+++ smart-caching/icn-protocol/service.test.js
@@ -3,11 +3,7 @@ const icnService = require('./service');
 
 const Content = require('./content');
 
-// afterAll(() => {
-//     icnService.close();
-// });
-
-test('POST /contents should be accepted', async () => {
+it('POST /contents should be accepted', async () => {
     payload = { value: 'val of k-1'};
     content = new Content('k', 1, 5, payload);
     await supertest(icnService)
