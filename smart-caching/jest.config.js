e1dfded802c48b04c2cbfe47cd871b804e1aadae
Hongwei Chen <hong.chen@futurewei.com>
2022-10-18 10:21:49 -0700

    minor: revised coverage config

--- smart-caching/jest.config.js
+++ smart-caching/jest.config.js
@@ -1,10 +1,12 @@
 const config = {
   verbose: false,
   collectCoverageFrom: [
-        "leaf/*.js",
-        "adapter/*.js",
-        "icn-protocol/*.js",
-	"!icn-protocol/routes.js"
+    "leaf/*.js",
+    "adapter/*.js",
+    "icn-protocol/*.js",
+    "!icn-protocol/routes.js",
+    "!leaf/leaf.js",
+    "!adapter/adapter.js",
   ],
 };
 
